a2c83a35754f2f5ece88593e0975008d7f64d030
MaximGromak <maximgromak@gmail.com>
2022-04-23 19:13:13 +1000

    change class to func

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,9 +1,9 @@
-import React, {Component} from "react";
+import React from "react";
 import Player from "./Player";
 
-export default class Widget extends Component {
+export default function Widget(props) {
 
-    videoArr = [
+    let videoArr = [
         {
             id: 0,
             v: "KHQr27xsOD0",
@@ -24,7 +24,7 @@ export default class Widget extends Component {
 
         {
             id: 3,
-            v: "xYAHb0QvxVk",date: new Date(2022, 3, 13, 1, 20, 30)
+            v: "xYAHb0QvxVk", date: new Date(2022, 3, 13, 1, 20, 30)
 
         },
 
@@ -42,40 +42,45 @@ export default class Widget extends Component {
 
     ]
 
-    constructor(props, context) {
-        super(props, context);
-    }
-
-    getBeautifulDate(d, id, url){
+    function getBeautifulDate(d, id, url) {
         let current = new Date();
 
         let timeDiff = Math.abs(current.getTime() - d.getTime());
         let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
         switch (diffDays) {
-            case 0: return <Player key={id} url={url} date='Видео загружено сегодня'/>
-            case 1: return <Player key={id} url={url} date='Видео загружено вчера'/>
-            case 2: return <Player key={id} url={url} date='Видео загружено 2 дня назад'/>
-            case 3: return <Player key={id} url={url} date='Видео загружено 3 дня назад'/>
-            case 4: return <Player key={id} url={url} date='Видео загружено 4 дня назад'/>
-            case 5: return <Player key={id} url={url} date='Видео загружено 5 дней назад'/>
-            case 6: return <Player key={id} url={url} date='Видео загружено 6 дней назад'/>
-            case 7: return <Player key={id} url={url} date='Видео загружено неделю назад'/>
-            default: return <Player key={id} url={url} date='Видео загружено давно...'/>
+            case 0:
+                return <Player key={id} url={url} date='Видео загружено сегодня'/>
+            case 1:
+                return <Player key={id} url={url} date='Видео загружено вчера'/>
+            case 2:
+                return <Player key={id} url={url} date='Видео загружено 2 дня назад'/>
+            case 3:
+                return <Player key={id} url={url} date='Видео загружено 3 дня назад'/>
+            case 4:
+                return <Player key={id} url={url} date='Видео загружено 4 дня назад'/>
+            case 5:
+                return <Player key={id} url={url} date='Видео загружено 5 дней назад'/>
+            case 6:
+                return <Player key={id} url={url} date='Видео загружено 6 дней назад'/>
+            case 7:
+                return <Player key={id} url={url} date='Видео загружено неделю назад'/>
+            default:
+                return <Player key={id} url={url} date='Видео загружено давно...'/>
         }
     }
 
-    render() {
-        let arr = [];
-        this.videoArr.forEach(video => {
-                arr.push(
-                    this.getBeautifulDate(video.date, video.id, video.v)
-                );
-            }
-        );
-        return (
-            <div className='list flex-row'>
-                {arr}
-            </div>
-        );
-    }
+
+    let arr = [];
+    videoArr.forEach(video => {
+            arr.push(
+                getBeautifulDate(video.date, video.id, video.v)
+            );
+        }
+    );
+    return (
+        <div className='list flex-row'>
+            {arr}
+        </div>
+    );
+
 }
